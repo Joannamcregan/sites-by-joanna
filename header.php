cd185a10708cb3db2f39494813fe036e3549ee5d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-23 17:27:18 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <meta name = "viewport" content = "width=device-width", initial-scale=1>
         <?php wp_head(); ?>
     </head>
-    <body class="light">
+    <body class="dark">
         <header>
             <div id="header--mobile-menu-icon">
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_dark.jpg'); ?>" aria-label="mobile menu" id="header--mobile-menu--dark" />
