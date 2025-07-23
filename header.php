7eeca20007e5b8258e47477f513723ecad03ef78
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-23 18:05:00 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -11,10 +11,9 @@
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_dark.jpg'); ?>" aria-label="mobile menu" id="header--mobile-menu--dark" />
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_light.jpg'); ?>" aria-label="mobile-menu" id="header--mobile-menu--light" />
             </div>
-            <span id="header--text-logo">Sites by Joanna</span>
             <div id="header--img-logo">
-                <img src="<?php echo get_theme_file_uri('/images/dark_mobile_logo.jpg'); ?>" alt="an illustration of a white mobile phone with a white letter J appearing on a dark screen" id="header--img-logo--dark" />
-                <img src="<?php echo get_theme_file_uri('/images/light_mobile_logo.jpg'); ?>" alt="an illustration of a black mobile phone with a black letter J appearing on a light screen" id="header--img-logo--light" />
+                <img src="<?php echo get_theme_file_uri('/images/dark_logo.jpg'); ?>" alt="an illustration of a white mobile phone with a white letter J appearing on a dark screen" id="header--img-logo--dark" />
+                <img src="<?php echo get_theme_file_uri('/images/light_logo.jpg'); ?>" alt="an illustration of a black mobile phone with a black letter J appearing on a light screen" id="header--img-logo--light" />
             </div>
             <nav>
                 <span>WordPress</span>
