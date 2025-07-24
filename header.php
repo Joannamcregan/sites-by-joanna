cdc0175228f78921b05ebe7ae65cb3d593ac0102
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-24 07:41:42 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -5,15 +5,15 @@
         <meta name = "viewport" content = "width=device-width", initial-scale=1>
         <?php wp_head(); ?>
     </head>
-    <body class="dark">
+    <body class="light">
         <header>
             <div id="header--mobile-menu-icon">
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_dark.jpg'); ?>" aria-label="mobile menu" id="header--mobile-menu--dark" />
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_light.jpg'); ?>" aria-label="mobile-menu" id="header--mobile-menu--light" />
             </div>
             <div id="header--img-logo">
-                <img src="<?php echo get_theme_file_uri('/images/dark_logo.jpg'); ?>" alt="an illustration of a white mobile phone with a white letter J appearing on a dark screen" id="header--img-logo--dark" />
-                <img src="<?php echo get_theme_file_uri('/images/light_logo.jpg'); ?>" alt="an illustration of a black mobile phone with a black letter J appearing on a light screen" id="header--img-logo--light" />
+                <img src="<?php echo get_theme_file_uri('/images/dark_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in white, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--dark" />
+                <img src="<?php echo get_theme_file_uri('/images/light_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in black, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--light" />
             </div>
             <nav>
                 <span>WordPress</span>
