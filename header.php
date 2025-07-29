53dd1dfcd13820c5280382102a8d9a7e32f24378
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-29 11:02:43 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -11,9 +11,8 @@
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_dark.jpg'); ?>" aria-label="mobile menu" id="header--mobile-menu--dark" />
                 <img src="<?php echo get_theme_file_uri('/images/mobile_menu_light.jpg'); ?>" aria-label="mobile-menu" id="header--mobile-menu--light" />
             </div>
-            <div id="header--img-logo">
-                <img src="<?php echo get_theme_file_uri('/images/dark_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in white, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--dark" />
-                <img src="<?php echo get_theme_file_uri('/images/light_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in black, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--light" />
+            <div id="header--mode-selector">
+
             </div>
             <nav>
                 <span>WordPress</span>
@@ -21,4 +20,8 @@
                 <span>Other Specialties</span>
                 <span>About</span>
             </nav>
+            <div id="header--img-logo">
+                <img src="<?php echo get_theme_file_uri('/images/dark_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in white, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--dark" />
+                <img src="<?php echo get_theme_file_uri('/images/light_logo.jpg'); ?>" alt="The words 'Sites by Joanna' appear in black, green, and purple letters in front of a simple, green, pink, and purple illustration of a 1990s desktop computer" id="header--img-logo--light" />
+            </div>
         </header>
