a7af966fce8381ebfefa09a96563b83978045e56
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-08-06 15:59:23 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 
 <main>
-    <h1 class="right-text vergillia">Custom WordPress Theme and Plugin Development</h1>
+    <h1 class="right-text vergillia">Crafting websites with a personal touch</h1>
+    <p>AI can do some really cool things (like <a href="http://sitesbyjoanna.local/" target="_blank">finding cures for rare diseases</a> and <a href="https://www.space.com/google-cloud-ai-tool-asteroid-telescope-archive" target="_blank">cataloging potentially dangerous asteroids</a>) but it sucks at making websites. While countless no-code page builders exist, they often result in sites that are slow and inaccessible for users with visual impairments and other special needs.</p>
 </main>
 
 <?php get_footer(); ?>
