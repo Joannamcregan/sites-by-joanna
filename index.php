0e52b417f209f66fe02dc507505c9cf12a5ee73d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-08-08 16:28:36 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main>
-    <h1 class="right-text vergillia">Crafting websites with a personal touch</h1>
-    <p>AI can do some really cool things (like <a href="http://sitesbyjoanna.local/" target="_blank">finding cures for rare diseases</a> and <a href="https://www.space.com/google-cloud-ai-tool-asteroid-telescope-archive" target="_blank">keeping tabs on asteroids</a>) but it sucks at making websites. While countless no-code page builders exist, they often produce sites that are slow and not fully accessible. Your business, organization, or personal passion deserve a unique online presence designed with the expertise and empathy that only a human can provide, and I just might be the human you need.</p>
+    <h1 class="right-text vergillia">Crafting and Optimizing One-of-a-Kind Websites</h1>
+    <p>Let's work together to give your site visitors a smooth, accessible, unique first impression. With professional experience and education in web coding, design, and UX, I have the skills needed to create a digital home that fits your style and budget.</p>
 </main>
 
 <?php get_footer(); ?>
